c07328f6f7ea6c5ec23a9ea37d2ad216572c1473
Boris Polyanskiy <mr.azarko@gmail.com>
2022-07-31 15:56:25 +0300

    add video_text handler for telegram bot (#3)
    * add video_text handler for telegram bot
    * add version to requirements.txt
    * little fixes

--- mbc/telegram_bot.py
+++ mbc/telegram_bot.py
@@ -1,10 +1,20 @@
+import asyncio
+import concurrent.futures
+import contextlib
+import io
 import logging
+import os
+import uuid
 
 import aiogram
 import aiogram.types
+import imaginator.entry as imaginator_entry
 
 
 logger = logging.getLogger(__name__)
+
+
+MAX_TEXT_SIZE = 150
 
 
 class Bot:
@@ -17,6 +27,9 @@ class Bot:
     async def on_startup(self):
         self.dispatcher.register_message_handler(
             self.start_handler, commands=['start'],
+        )
+        self.dispatcher.register_message_handler(
+            self.video_text_handler, commands=['video_text']
         )
         if self.webhook_url:
             webhook = await self.bot.get_webhook_info()
@@ -33,7 +46,7 @@ class Bot:
             else:
                 logger.info(f'webhook already set: {self.webhook_url}')
         else:
-            logger.info('webhook is not configured, skip')
+            logger.warning('webhook is not configured, skip')
 
     async def on_shutdown(self, *args, **kwargs):
         if self.webhook_url:
@@ -42,3 +55,45 @@ class Bot:
 
     async def start_handler(self, message: aiogram.types.Message):
         return await message.reply('started')
+
+    async def video_text_handler(self, message: aiogram.types.Message):
+        command, command_argument = message.get_full_command()
+        if not command_argument:
+            logger.info(f'got {command} without arguments')
+            return await message.reply('input text after command, please')
+        if len(command_argument) > MAX_TEXT_SIZE:
+            logger.info('len of received text for animation too long')
+            return await message.reply(
+                f'Your message too long! Max message size is {MAX_TEXT_SIZE}',
+            )
+        logger.info(f'generate video for text: {command_argument}')
+        loop = asyncio.get_event_loop()
+        with concurrent.futures.ThreadPoolExecutor() as pool:
+            content = await loop.run_in_executor(
+                pool, generate_video, command_argument,
+            )
+        await message.answer_chat_action(action='upload_video')
+        with io.BytesIO(content) as stream:
+            stream.name = 'animation.mp4'
+            return await message.answer_animation(animation=stream)
+
+
+def generate_video(text: str) -> bytes:
+    logger.info(f'start generating video for text "{text}')
+    filename = f'{uuid.uuid4().hex}.mp4'
+    try:
+        imaginator = imaginator_entry.Imaginator()
+        imaginator_entry.create_video(
+            imaginator=imaginator,
+            name=filename,
+            text_line=text,
+        )
+        return _load_result(filename)
+    finally:
+        with contextlib.suppress(OSError):
+            os.remove(filename)
+
+
+def _load_result(path: str) -> bytes:
+    with open(path, 'rb') as stream:
+        return stream.read()
--- tests/test_index.py
+++ tests/test_index.py
@@ -2,7 +2,7 @@ import http
 
 
 async def test_hello(web_app_client):
-    response = await web_app_client.get("/")
+    response = await web_app_client.get('/')
     assert response.status == http.HTTPStatus.OK
     text = await response.text()
-    assert "Hello, world" in text
+    assert 'Hello, world' in text
