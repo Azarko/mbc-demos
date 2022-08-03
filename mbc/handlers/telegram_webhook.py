25cd1c23dfe857fb26009352afa63105f6030101
azarko <mr.azarko@gmail.com>
2022-08-03 21:58:29 +0300

    process TelegramAPIError exception

--- mbc/handlers/telegram_webhook.py
+++ mbc/handlers/telegram_webhook.py
@@ -14,12 +14,12 @@ logger = logging.getLogger(__name__)
 async def handler(request: types.Request):
     update = await request.json()
     update = aiogram.types.Update(**update)
-    logger.info(f'fetched webhook: {update}')
     aiogram.Bot.set_current(request.app.bot.bot)
     try:
         await request.app.bot.dispatcher.process_update(update)
-    except aiogram_exceptions.BotBlocked as err:
+    except aiogram_exceptions.TelegramAPIError as err:
         logger.warning(
-            'fetched command from user which blocked bot', exc_info=err,
+            f'api exception raised while process update: {update}',
+            exc_info=err,
         )
     return web.json_response({})
