14ac78cff9f23cb5c1107fba0941a9ed852dc6e0
azarko <mr.azarko@gmail.com>
2022-08-03 21:08:32 +0300

    add more logs

--- mbc/handlers/telegram_webhook.py
+++ mbc/handlers/telegram_webhook.py
@@ -1,3 +1,5 @@
+import logging
+
 import aiogram
 import aiogram.types
 from aiohttp import web
@@ -5,9 +7,13 @@ from aiohttp import web
 from mbc import types
 
 
+logger = logging.getLogger(__name__)
+
+
 async def handler(request: types.Request):
     update = await request.json()
     update = aiogram.types.Update(**update)
+    logger.info(f'fetched webhook: {update}')
     aiogram.Bot.set_current(request.app.bot.bot)
     await request.app.bot.dispatcher.process_update(update)
     return web.json_response({})
