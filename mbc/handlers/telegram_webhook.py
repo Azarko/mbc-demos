121ee93a1644ce53d2dab33815dede91b734fb37
Boris Polyanskiy <mr.azarko@gmail.com>
2022-08-03 22:01:50 +0300

    process telegram api exception (#7)
    process TelegramAPIError exception

--- mbc/handlers/telegram_webhook.py
+++ mbc/handlers/telegram_webhook.py
@@ -1,13 +1,25 @@
+import logging
+
 import aiogram
 import aiogram.types
+from aiogram.utils import exceptions as aiogram_exceptions
 from aiohttp import web
 
 from mbc import types
+
+
+logger = logging.getLogger(__name__)
 
 
 async def handler(request: types.Request):
     update = await request.json()
     update = aiogram.types.Update(**update)
     aiogram.Bot.set_current(request.app.bot.bot)
-    await request.app.bot.dispatcher.process_update(update)
+    try:
+        await request.app.bot.dispatcher.process_update(update)
+    except aiogram_exceptions.TelegramAPIError as err:
+        logger.warning(
+            f'api exception raised while process update: {update}',
+            exc_info=err,
+        )
     return web.json_response({})
